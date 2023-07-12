e93d743f7db983fd0b9c4ade130223c91c0af493
Illya <ilya.solovev.88@bk.ru>
2023-07-12 19:11:45 +0300

    feature a playVideo module

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from "./modules/slider";
+import Videoplayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const player = new Videoplayer('.play', '.overlay');
+    player.init();
 });
