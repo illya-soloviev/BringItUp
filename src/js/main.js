a8c98aeecb08cff70caddac78adbf059047006b7
Illya <ilya.solovev.88@bk.ru>
2023-07-15 18:12:42 +0300

    feature a difference module

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import Videoplayer from "./modules/playVideo";
+import Difference from "./modules/difference";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next'});
@@ -23,4 +24,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new Videoplayer('.play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 });
